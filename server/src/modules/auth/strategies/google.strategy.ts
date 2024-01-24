0e20e04fc1a091b10a6b44cc42c9fa24b6afd35c
romelgomez <bmxandcode@gmail.com>
2024-01-24 16:00:40 -0500

    chore: it return the refresh token

--- server/src/modules/auth/strategies/google.strategy.ts
+++ server/src/modules/auth/strategies/google.strategy.ts
@@ -6,11 +6,19 @@ import { ProcessEnvEnum } from 'src/modules/config/config.types';
 import { AuthService } from '../auth.service';
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy) {
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(
     protected appConfigService: AppConfigService,
     protected authService: AuthService,
   ) {
+    //
+    // Set authorization parameters
+    // ref:
+    //  - https://developers.google.com/identity/protocols/oauth2/web-server#node.js
+    //  - https://developers.google.com/identity/protocols/OAuth2WebServer
+    //  - /node_modules/passport-google-oauth20/lib/strategy.js
+    //  - https://github.com/nestjs/passport/issues/57
+    //
     super({
       clientID: appConfigService.getEnvVal(ProcessEnvEnum.GOOGLE_CLIENT_ID),
       clientSecret: appConfigService.getEnvVal(
@@ -23,7 +31,22 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(accessToken, refreshToken, profile, done) {
+  //
+  // ref:
+  // - https://github.com/nestjs/passport/issues/57#issuecomment-510610374
+  //
+  authenticate(req, options: any): any {
+    super.authenticate(
+      req,
+      Object.assign(options, {
+        session: true,
+        accessType: 'offline',
+        includeGrantedScopes: true,
+      }),
+    );
+  }
+
+  async validate(accessToken, refreshToken, profile, cb) {
     const { name, emails } = profile;
 
     console.log('profile', profile);
